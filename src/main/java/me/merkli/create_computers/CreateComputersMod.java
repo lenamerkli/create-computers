64794d54501994d383260f11d0776b7d730c6f98
Lena Merkli <lena.merkli@protonmail.com>
2023-10-08 16:37:33 +0200

    Initial commit

---
+++ src/main/java/me/merkli/create_computers/CreateComputersMod.java
@@ -0,0 +1,30 @@
+package me.merkli.create_computers;
+
+import com.simibubi.create.Create;
+
+import io.github.fabricators_of_create.porting_lib.util.EnvExecutor;
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.ResourceLocation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CreateComputersMod implements ModInitializer {
+	public static final String ID = "create_computers";
+	public static final String NAME = "Create Computers";
+	public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Create addon mod [{}] is loading alongside Create [{}]!", NAME, Create.VERSION);
+		LOGGER.info(EnvExecutor.unsafeRunForDist(
+				() -> () -> "{} is accessing Porting Lib from the client!",
+				() -> () -> "{} is accessing Porting Lib from the server!"
+		), NAME);
+	}
+
+	public static ResourceLocation id(String path) {
+		return new ResourceLocation(ID, path);
+	}
+}
